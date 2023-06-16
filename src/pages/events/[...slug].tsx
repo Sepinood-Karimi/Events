fa804d4b976657a93a3449061d77b476306ed9e0
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-06-16 13:59:43 +0330

    add initial files

---
+++ src/pages/events/[...slug].tsx
@@ -0,0 +1,4 @@
+const FilteredEventsPage = () => {
+  return <h1> filtered events </h1>;
+};
+export default FilteredEventsPage;
---
+++ src/pages/events/index.tsx
@@ -0,0 +1,5 @@
+const EventsPage = () => {
+  return <h1>events page</h1>;
+};
+
+export default EventsPage;
