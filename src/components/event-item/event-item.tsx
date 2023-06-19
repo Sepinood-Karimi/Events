7c380a7a2cec1ea31dcdabe5901a6bd5a41ffb36
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2023-06-19 10:52:05 +0330

    add icons and styles

--- src/components/event-item/event-item.tsx
+++ src/components/event-item/event-item.tsx
@@ -3,6 +3,9 @@ import eventObj from "@/models/eventObj";
 import classnames from "classnames";
 import classes from "./event-item.module.css";
 import Button from "@/components/UI/Button/button";
+import Location from "@/components/UI/Icons/location";
+import Calender from "@/components/UI/Icons/Calender";
+import RightArrow from "@/components/UI/Icons/right-arrow";
 
 const EventItem: React.FC<{ eventItem: eventObj }> = (props) => {
   const humanReadableDate = new Date(props.eventItem.date).toLocaleDateString(
@@ -22,14 +25,19 @@ const EventItem: React.FC<{ eventItem: eventObj }> = (props) => {
         <div className={classnames(classes.title)}>
           <h2>{props.eventItem.title}</h2>
           <div className={classnames(classes.date)}>
+            <Calender />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classnames(classes.address)}>
+            <Location />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classnames(classes.link)}>
-          <Button link={exploreLink} children="Explore Event" />
+          <Button link={exploreLink}>
+            <span> Explore Event </span>
+            <RightArrow />
+          </Button>
         </div>
       </div>
     </li>
